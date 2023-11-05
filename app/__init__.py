33667de1b3153357567ac7e56722b49ef07de93b
Airplanejuju <88552693+Airplanejuju@users.noreply.github.com>
2023-11-05 20:29:43 +0800

    Modify SQL connection

--- app/__init__.py
+++ app/__init__.py
@@ -1,33 +1,27 @@
 from flask import Flask
 from config import DB_USERNAME, DB_PASSWORD, DB_NAME, DB_HOST, SECRET_KEY, BOOTSTRAP_SERVE_LOCAL
 from flask_wtf.csrf import CSRFProtect
-import mysql.connector
+from mysql.connector import connect
 
 def create_app(test_config=None):
-    app = Flask(__name__, instance_relative_config=True)
+    app = Flask(__name__)
     app.config.from_mapping(
         SECRET_KEY=SECRET_KEY,
         MYSQL_USER=DB_USERNAME,
         MYSQL_PASSWORD=DB_PASSWORD,
         MYSQL_DATABASE=DB_NAME,
         MYSQL_HOST=DB_HOST,
-        #BOOTSTRAP_SERVE_LOCAL=BOOTSTRAP_SERVE_LOCAL
+        BOOTSTRAP_SERVE_LOCAL=BOOTSTRAP_SERVE_LOCAL
     )
     CSRFProtect(app)
-
-    # Create a MySQL connection
-    db = mysql.connector.connect(
-        user=app.config['MYSQL_USER'],
-        password=app.config['MYSQL_PASSWORD'],
-        host=app.config['MYSQL_HOST'],
-        database=app.config['MYSQL_DATABASE']
+    
+    # Centralized database connection
+    app.db_connection = connect(
+        user=DB_USERNAME,
+        password=DB_PASSWORD,
+        host=DB_HOST,
+        database=DB_NAME
     )
-
-    # Add the MySQL connection to the Flask app context
-    app.config['MYSQL_DB'] = db
-
-    # Create a cursor to execute MySQL queries
-    #cursor = db.cursor()
 
     from .routes.college import college_bp as college_blueprint
     app.register_blueprint(college_blueprint, url_prefix='/college')
