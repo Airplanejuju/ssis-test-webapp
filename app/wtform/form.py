70c99637f0539a1b194166f6d6f1764d7ee4503a
Airplanejuju <88552693+Airplanejuju@users.noreply.github.com>
2023-12-07 04:34:00 +0800

    Add photo file field
    Add changes on select choices

--- app/wtform/form.py
+++ app/wtform/form.py
@@ -1,7 +1,6 @@
 from flask_wtf import FlaskForm
-from wtforms import StringField, SubmitField, SelectField#, IntegerField
-from wtforms.validators import DataRequired, Length#, ValidationError
-
+from wtforms import StringField, SubmitField, SelectField, FileField#, IntegerField
+from wtforms.validators import DataRequired, Length
 
 class StudentForm(FlaskForm):
     id = StringField('ID', validators=[DataRequired(message='Please input your ID.'),Length(min=9, max=9)])
@@ -11,40 +10,42 @@ class StudentForm(FlaskForm):
     lastname = StringField('LastName', validators=[DataRequired(message='Please input your last name.'), 
                             Length(min=1, max=45)])
     
-    gender_choices = [('', ''),('F','Female'),('M','Male'),('Other','Other'),('Prefer not to say','Prefer not to say')]
-    gender = SelectField('Gender', choices=gender_choices, validators=[DataRequired()])
+    gender_choices = [('', '-- Please select --', {'disabled': True}),('F','Female'),('M','Male'),('Other','Other'),('Prefer not to say','Prefer not to say')]
+    gender = SelectField('Gender', choices=gender_choices, validators=[DataRequired()], default='')
 
-    college_choices = [('', ''),('College of Engineering','College of Engineering'),('College of Arts and Social Sciences','College of Arts and Social Sciences'),
+    college_choices = [('', '-- Please select --', {'disabled': True}),('College of Engineering','College of Engineering'),('College of Arts and Social Sciences','College of Arts and Social Sciences'),
                         ('College of Economics and Business Administration','College of Economics and Business Administration'),('College of Education','College of Education'),
                         ('College of Health Sciences','College of Health Sciences'),('College of Science and Mathematics','College of Science and Mathematics'),
                         ('College of Computer Studies', 'College of Computer Studies')]
-    college = SelectField('College', choices=college_choices, validators=[DataRequired()])
+    college = SelectField('College', choices=college_choices, validators=[DataRequired()], default='')
     
     # The course choices will be dynamically updated based on the selected college
     course = SelectField('Course', choices=[], validators=[DataRequired()])
 
-    year_choices = [('', ''),('1','1st Year'),('2','2nd Year'),('3','3rd Year'),('4','4th Year')]
-    year = SelectField('Year', choices=year_choices, validators=[DataRequired()])
+    year_choices = [('', '-- Please select --', {'disabled': True}),('1','1st Year'),('2','2nd Year'),('3','3rd Year'),('4','4th Year')]
+    year = SelectField('Year', choices=year_choices, validators=[DataRequired()], default='')
+
+    photo = FileField('Upload Photo')
 
     submit = SubmitField('Submit')
 
 class CourseForm(FlaskForm):
-    college_choices = [('', ''),('College of Engineering','College of Engineering'),('College of Arts and Social Sciences','College of Arts and Social Sciences'),
+    college_choices = [('', '-- Please select --', {'disabled': True}),('College of Engineering','College of Engineering'),('College of Arts and Social Sciences','College of Arts and Social Sciences'),
                         ('College of Economics and Business Administration','College of Economics and Business Administration'),('College of Education','College of Education'),
                         ('College of Health Sciences','College of Health Sciences'),('College of Science and Mathematics','College of Science and Mathematics'),
                         ('College of Computer Studies', 'College of Computer Studies')]
-    college = SelectField('College', choices=college_choices, validators=[DataRequired()])
+    college = SelectField('College', choices=college_choices, validators=[DataRequired()], default='')
 
     # The course choices will be dynamically updated based on the selected college
-    course = SelectField('Course', choices=[], validators=[DataRequired()])
+    course = SelectField('Course', choices=[], validators=[DataRequired()], default='')
 
     submit = SubmitField('Submit')
 
 class CollegeForm(FlaskForm):
-    college_choices = [('', ''),('College of Engineering','College of Engineering'),('College of Arts and Social Sciences','College of Arts and Social Sciences'),
+    college_choices = [('', '-- Please select --', {'disabled': True}),('College of Engineering','College of Engineering'),('College of Arts and Social Sciences','College of Arts and Social Sciences'),
                         ('College of Economics and Business Administration','College of Economics and Business Administration'),('College of Education','College of Education'),
                         ('College of Health Sciences','College of Health Sciences'),('College of Science and Mathematics','College of Science and Mathematics'),
                         ('College of Computer Studies', 'College of Computer Studies')]
-    college = SelectField('College', choices=college_choices, validators=[DataRequired()])
+    college = SelectField('College', choices=college_choices, validators=[DataRequired()], default='')
 
     submit = SubmitField('Submit')
