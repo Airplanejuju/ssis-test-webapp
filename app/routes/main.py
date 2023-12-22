978310a06e0225dee2fc9cfd2d9aaf09be675960
Airplanejuju <88552693+Airplanejuju@users.noreply.github.com>
2023-12-22 14:41:35 +0800

    Modify route to student table

--- app/routes/main.py
+++ app/routes/main.py
@@ -1,7 +1,9 @@
 from flask import Blueprint, flash
 from flask import render_template, request, redirect
-from app.model.student_model import submit_form
+from app.model.student_model import submit_form, query_database
 from app.wtform.form import StudentForm
+
+from app.model.course_model import query_database as query_course
 
 main_bp = Blueprint(
     "main_bp",
@@ -10,19 +12,27 @@ main_bp = Blueprint(
 
 @main_bp.route('/')
 def home():
+    # form = StudentForm()
+    # return render_template('home.html', form=form)
+    data = query_database()
+
+    # Print the data for debugging purposes
+    print("Data from query:", data)
+
     form = StudentForm()
-    return render_template('home.html', form=form)
+    courses = query_course() #get courses available in course table
+    return render_template('table/student.html', data=data, form=form, courses=courses)
 
-@main_bp.route('/submit', methods=['POST'])
-def submit():
-    if request.method == 'POST':
-        try:
-            submit_form()
+# @main_bp.route('/submit', methods=['POST'])
+# def submit():
+#     if request.method == 'POST':
+#         try:
+#             submit_form()
 
-            # Flash a success message
-            flash('Submitted successfully!', 'success')
-        except Exception as e:
-            # Flash an error message
-            flash(f'Submission failed: {str(e)}', 'danger')
+#             # Flash a success message
+#             flash('Submitted successfully!', 'success')
+#         except Exception as e:
+#             # Flash an error message
+#             flash(f'Submission failed: {str(e)}', 'danger')
     
-    return redirect(request.referrer)
+#     return redirect(request.referrer)
